f48248334a9f84a141ab36556e8217d0604934eb
Lane Aasen <laneaasen@gmail.com>
2012-09-20 08:24:51 -0700

    a little more styling. added subtlepattern.

--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Lane Aasen</title>
-		<link rel="stylesheet" href="main.css">
+		<link rel="stylesheet" href="assets/css/main.css">
 	</head>
 
 	<body>
-		<p>hello</p>
+		<div class="box">
+			<h1>Lane Aasen</h1>
+			<h3>developer &middot; entrepeneur &middot; student</h3>
+			<p>I'm a dev from seattle and stuff</p>
+
+			<div class="social">
+				<a href="https://plus.google.com/100962461815374238974"><img src="assets/img/gplus.png" /></a>
+				<a href="https://twitter.com/laneaasen"><img src="assets/img/twitter.png" /></a>
+				<a href="https://www.facebook.com/lane.aasen"><img src="assets/img/facebook.png" /></a>
+				<a href="https://github.com/laneaasen"><img src="assets/img/github.png" /></a>
+			</div>
 	</body>
 </html>
+
