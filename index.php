d12c7437fcf0c0659255b644cef2baf08e3dddab
Lane Aasen <laneaasen@gmail.com>
2012-10-22 23:36:18 -0700

    corrects twitter handle

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 			<div class="social">
 				<a href="https://plus.google.com/100962461815374238974"><img src="assets/img/gplus.png" /></a>
-				<a href="https://twitter.com/laneaasen"><img src="assets/img/twitter.png" /></a>
+				<a href="https://twitter.com/aaaasen"><img src="assets/img/twitter.png" /></a>
 				<a href="https://www.facebook.com/lane.aasen"><img src="assets/img/facebook.png" /></a>
 				<a href="https://github.com/laneaasen"><img src="assets/img/github.png" /></a>
 				<a href="http://www.linkedin.com/pub/lane-aasen/54/b17/212"><img src="assets/img/linkedin.png" /></a>
