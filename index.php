d256a4c0b93bc5e4ab9dcc5b12a5a3c5ebcac3fe
Lane Aasen <laneaasen@gmail.com>
2012-11-06 23:43:32 -0800

    changes github username to aaasen

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 				<a href="https://plus.google.com/100962461815374238974"><img src="assets/img/gplus.png" /></a>
 				<a href="https://twitter.com/aaaasen"><img src="assets/img/twitter.png" /></a>
 				<a href="https://www.facebook.com/lane.aasen"><img src="assets/img/facebook.png" /></a>
-				<a href="https://github.com/laneaasen"><img src="assets/img/github.png" /></a>
+				<a href="https://github.com/aaasen"><img src="assets/img/github.png" /></a>
 				<a href="http://www.linkedin.com/pub/lane-aasen/54/b17/212"><img src="assets/img/linkedin.png" /></a>
 				<a href="mailto:laneaasen@gmail.com"><img src="assets/img/email.png" /></a>
 			</div>
