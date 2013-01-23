6ceffca4492b028e626db37fe2e73310d65e9412
Lane Aasen <laneaasen@gmail.com>
2013-01-22 20:56:47 -0800

    updated to a much simpler page

--- index.php
+++ index.php
@@ -11,16 +11,12 @@
 	<body>
 		<div class="box">
 			<h1>Lane Aasen</h1>
-			<h3>developer &middot; entrepeneur &middot; designer</h3>
-			<p>Cofounder at <a href="http://firedove.co">Firedove</a>, web dev on <a href="http://www.showrapp.co">Showr</a>. Avid hiker, skier and runner.</p>
+			<h3>- software adventurer -</h3>
 
 			<div class="social">
-				<a href="https://plus.google.com/100962461815374238974"><img src="assets/img/gplus.png" /></a>
-				<a href="https://twitter.com/aaaasen"><img src="assets/img/twitter.png" /></a>
-				<a href="https://www.facebook.com/lane.aasen"><img src="assets/img/facebook.png" /></a>
-				<a href="https://github.com/aaasen"><img src="assets/img/github.png" /></a>
-				<a href="http://www.linkedin.com/pub/lane-aasen/54/b17/212"><img src="assets/img/linkedin.png" /></a>
-				<a href="mailto:laneaasen@gmail.com"><img src="assets/img/email.png" /></a>
+				<a href="https://github.com/aaasen">github</a> &middot;
+				<a href="http://www.linkedin.com/pub/lane-aasen/54/b17/212">linkedin</a> &middot;
+				<a href="https://twitter.com/aaaasen">twitter</a>
 			</div>
 		</div>
 	</body>
